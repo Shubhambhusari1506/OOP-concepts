02c0f69fe3c278f73bace99cd6ed2de129944d5f
shubhambhusari <shubhambhusari2@gmail.com>
2021-02-24 12:04:29 +0530

    Added:line problem using equal and compareTo method

--- Linecomparision.java
+++ Linecomparision.java
@@ -2,33 +2,40 @@ import java.util.Scanner;
 
  class Linecomparision {
 
-		static Double length;
-		static Double length1;
+		static Double length, length1;
+		static int x1,y1,x2,y2,w1,z1,w2,z2;
 		
 
-		 public static double  length (int x1,int y1,int x2, int y2, int w1, int z1,int w2,int z2) {
+		 public static void  length () {
 		
 		length = Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
 		length1 = Math.sqrt(((w2 - w1) * (w2 - w1)) + ((z2 - z1) * (z2 - z1)));
 
-		return length;		
+		
 }
 
 
 
 		static void compare() {
-		int result = length.compareTo(length1);
-		if(result == 0) {
+		boolean result = length.equals(length1);
+
+		int compare = length.compareTo(length1);
+		if(result) {
 		System.out.println("Line1 and Line2 are equal in Length");
-			}else {
-		System.out.println("Line1 and Line2 are not equal in Length");
+		}
+		else {
+		if (compare > 0){		
+		System.out.println("Line1 is greater than Line2 are  equal in Length");
 	
 
-	}
+		}
+		else{
+		System.out.println("Line1 is less than Line2 are  equal in Length");
+		}
 
+ }
 }
-
-		public static  void main (String []args ){
+		public static void input () {
 
 		int x1 , y1 , x2, y2;
 		int w1, z1, w2,z2;
@@ -55,15 +62,19 @@ import java.util.Scanner;
          w2 = s.nextInt();
          System.out.println("Enter z2: ");
          z2 = s.nextInt();
+						
+			}
 
+			
+			public static void main(String [] args){
 
-			length(x1 ,y1, x2, y2, w1,z1,w2,z2);
-
+			input();
+			length();
 			System.out.println("Length of Line1: "+length);
 			System.out.println("length of line2:"+length1);
 
 				compare();
 
 	}
-}
+ }
 
