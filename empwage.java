ccd4bdfa0d34858e06590d39d192097959b897e6
shubhambhusari <shubhambhusari2@gmail.com>
2021-02-24 12:25:26 +0530

    Added:Cal daily emp wage

--- empwage.java
+++ empwage.java
@@ -1,25 +1,32 @@
 public class empwage {
 
-	//Constant
-	public static final int IS_PRESENT = 1;
+		//Constant
+		public static final int IS_PRESENT = 1;
+		public static final int emp_wage_per_hr = 20;
 
+		public static void calwage(){
 
-	public static void checkAttendance(){
-
-
+		//variables
+		int emphrs,empwage;
 		int empCheck =(int) Math.floor(Math.random() * 10) % 2;
 		switch (empCheck) {
 		case IS_PRESENT :
 		System.out.println("Employee is Present");
+		emphrs = 8;
 		break;
 		default :
 		System.out.println("Employee is Absent");
+		emphrs = 0;
 		}
+		empwage = emphrs * emp_wage_per_hr;
+		System.out.println("employee  daily wage  " +empwage);
+
 	}
+
 
 		public static void main(String[] args){
 
 	
- 		checkAttendance();
+ 		calwage();
 	}		
 }	
