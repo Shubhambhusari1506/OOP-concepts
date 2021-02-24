1c71f60f1d67471d43e645057c8d0f040ffe23f3
shubhambhusari <shubhambhusari2@gmail.com>
2021-02-24 13:16:54 +0530

    Added:cal wages for month

--- empwage.java
+++ empwage.java
@@ -7,47 +7,51 @@ public class empwage {
 		public static final int IS_PART_TIME = 2;
 		
 		public static final int emp_wage_per_hr = 20;
-
-		public static void calwage(){
-
-		//variables
-		int emphrs,empwage;
 		
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+		public static final int days_in_month = 20;
+		
+		static int totalempwage;
+		
+		public static int calwage(){
+		
+		for (int day = 1; day <= days_in_month; day++){
+		
+		//variables
+		
+		int emphrs, empwage; 
+		
+		int empCheck =(int) Math.floor(Math.random() * 10) % 3;
 		
 		if (empCheck == IS_FULL_TIME) {
-
-		System.out.println("employee is full time");
-
-		emphrs = 8;
+	
+			emphrs = 8;
+	
+		}else if (empCheck == IS_PART_TIME) {
 		
-		}else if 
-
-		(empCheck == IS_PART_TIME){
-
-		System.out.println("employee is part time");
-
+	
 		emphrs = 4;
-
-		}
-
-		else {
-
-		System.out.println("employee is absent");
-
+	
+		}else {
+	
 		emphrs = 0;
-		}
-
-		empwage = emphrs * emp_wage_per_hr;
-		
-		System.out.println("employee  daily wage  " +empwage);
 
 	}
+	
+		empwage = emphrs * emp_wage_per_hr;
+	
+			totalempwage += empwage;
+	
+			System.out.println("Employee day " + day + " Wage : "+empwage);
+	
+		}
+		
+		return totalempwage;
 
-
+}
 		public static void main(String[] args){
 
 	
  		calwage();
+		System.out.println("Total Employee Wage : " +totalempwage);
 	}		
 }	
