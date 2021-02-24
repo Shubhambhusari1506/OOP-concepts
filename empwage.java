0e39bb3ed8ff935a8dd821f16d81d3baeb511e58
shubhambhusari <shubhambhusari2@gmail.com>
2021-02-24 12:15:18 +0530

    Added:empwage for attaindence check

---
+++ empwage.java
@@ -0,0 +1,25 @@
+public class empwage {
+
+	//Constant
+	public static final int IS_PRESENT = 1;
+
+
+	public static void checkAttendance(){
+
+
+		int empCheck =(int) Math.floor(Math.random() * 10) % 2;
+		switch (empCheck) {
+		case IS_PRESENT :
+		System.out.println("Employee is Present");
+		break;
+		default :
+		System.out.println("Employee is Absent");
+		}
+	}
+
+		public static void main(String[] args){
+
+	
+ 		checkAttendance();
+	}		
+}	
